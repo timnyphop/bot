5319b2e464a8dfd42192e740993707bc0500b69b
tinpo <137786541+timnyphop@users.noreply.github.com>
2023-09-21 16:15:00 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,23 @@
+import telebot
+
+botTimeWeb = telebot.TeleBot('6472706302:AAHIGwDHNt6GbyoE5cXKDgQy5kAyH5S0RZc')
+
+from telebot import types
+@botTimeWeb.message_handler(commands=['start'])
+def startBot(message):
+  first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь расскажу немного о нашей компании?"
+  markup = types.InlineKeyboardMarkup()
+  button_yes = types.InlineKeyboardButton(text = 'Да', callback_data='yes')
+  markup.add(button_yes)
+  botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+
+  @botTimeWeb.callback_query_handler(func=lambda call: True)
+  def response(function_call):
+      if function_call.message:
+          if function_call.data == "yes":
+              second_mess = "Мы магазин одежды, наши основатели крутые гайсы!"
+              markup = types.InlineKeyboardMarkup()
+              markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://web.telegram.org/a/#-995440557"))
+              botTimeWeb.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
+              botTimeWeb.answer_calзнерщтlback_query(function_call.id)
+botTimeWeb.infinity_polling()
